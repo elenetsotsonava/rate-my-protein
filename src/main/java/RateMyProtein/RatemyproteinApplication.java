1357dff3342ceb8bbc4794eb363705e0227e3887
elenetsotsonava <tsotsonavae@gmail.com>
2026-07-21 00:00:52 +0400

    Initialize RateMyProtein Spring Boot application

---
+++ src/main/java/RateMyProtein/RatemyproteinApplication.java
@@ -0,0 +1,13 @@
+package RateMyProtein;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatemyproteinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatemyproteinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/RateMyProtein/RatemyproteinApplicationTests.java
@@ -0,0 +1,13 @@
+package RateMyProtein;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatemyproteinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
