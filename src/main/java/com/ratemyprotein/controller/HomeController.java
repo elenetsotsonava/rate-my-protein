1cf364fcf38abab1b238d350b62f1e51a9500037
elenetsotsonava <tsotsonavae@gmail.com>
2026-07-22 03:21:37 +0400

    Redesign homepage with featured products

--- src/main/java/com/ratemyprotein/controller/HomeController.java
+++ src/main/java/com/ratemyprotein/controller/HomeController.java
@@ -1,13 +1,36 @@
 package com.ratemyprotein.controller;
 
+import com.ratemyprotein.entity.Product;
+import com.ratemyprotein.service.ProductService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
+    private final ProductService productService;
+
+    public HomeController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping("/")
-    public String home() {
+    public String showHomePage(Model model) {
+
+        List<Product> featuredProducts = productService
+                .getActiveProducts()
+                .stream()
+                .limit(3)
+                .toList();
+
+        model.addAttribute(
+                "featuredProducts",
+                featuredProducts
+        );
+
         return "index";
     }
 }
