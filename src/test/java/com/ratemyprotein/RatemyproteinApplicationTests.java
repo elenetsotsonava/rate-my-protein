fed38605736f743f0364a5cf7ce854086cb73db9
elenetsotsonava <tsotsonavae@gmail.com>
2026-07-23 00:41:05 +0400

    Prepare application for deployment

--- src/test/java/com/ratemyprotein/RatemyproteinApplicationTests.java
+++ src/test/java/com/ratemyprotein/RatemyproteinApplicationTests.java
@@ -1,13 +1,12 @@
-package RateMyProtein;
+package com.ratemyprotein;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = RatemyproteinApplication.class)
 class RatemyproteinApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
